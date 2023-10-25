f7e16a8ed38ffde5708dfdcba0578e2e22dc7f5b
DreamingCodes <me@dreaming.codes>
2023-10-25 13:38:29 +0200

    Add multi-client support and game enhancement
    Migrated the server from single-client to multi-client environment. For handling multiple clients simultaneously, a thread-safe CopyOnWriteArrayList was used to maintain active clients and a new class 'ClientHandler' was created to manage client-related functionalities. Specific functions to broadcast a message to all clients and to remove a client from the list were added within Server.java.
    
    In the gaming section, real-time updates were provided to the game of guessing a number. A separate listener thread in Client.java was used to cope with server messages, giving a prompt to interact with the user when their input is required. It results in users being notified instantly whenever any player wins the game.
    
    This update improves game interactivity and engagement, bringing a competitive aspect to gameplay.

--- src/main/java/codes/dreaming/Client.java
+++ src/main/java/codes/dreaming/Client.java
@@ -4,29 +4,47 @@ import java.io.*;
 import java.net.*;
 
 public class Client {
+    private boolean isWinAnnounced = false;
+
     public void start(String ip, int port) {
-        // Try with resources construct to ensure everything is properly closed
         try (Socket echoSocket = new Socket(ip, port);
              PrintWriter out = new PrintWriter(echoSocket.getOutputStream(), true);
              BufferedReader in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
              BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in))
         ) {
-            String userInput;
-            System.out.print("Inserisci il numero: ");
-            for (int i = 1; (userInput = stdIn.readLine()) != null; i++) {
-                out.println(userInput);
-                int serverResponse = Integer.parseInt(in.readLine());
+            // Start a new thread that will listen for the winning message from the server
+            Thread listenerThread = new Thread(() -> {
+                try {
+                    while (!Thread.currentThread().isInterrupted()) {
+                        String serverResponse = in.readLine();
+                        switch (Integer.parseInt(serverResponse)){
+                            case 1 -> System.out.println("Il numero è troppo piccolo");
+                            case 2 -> System.out.println("Il numero è troppo grande");
+                            case 3 -> {
+                                System.out.println("Hai indovinato!");
+                                System.exit(0);
+                            }
+                            case 4 -> {
+                                System.out.println("Hai perso, qualcuno ha indovinato prima di te!");
+                                System.exit(0);
+                            }
+                        }
 
-                if (serverResponse == 1)
-                    System.out.println("Numero è troppo piccolo");
-                else if (serverResponse == 2)
-                    System.out.println("Numero è troppo grande");
-                else {
-                    System.out.println("HAI INDOVINATO, IN " + i + " TENTATIVI!");
-                    break;
+                        System.out.print("Inserisci il numero: ");
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-                System.out.print("Inserisci il numero: ");
+            });
+
+            listenerThread.start(); // Start the listener thread
+
+            System.out.print("Inserisci il numero: ");
+            for (int i = 1; !isWinAnnounced; i++) {
+                String userInput = stdIn.readLine();
+                out.println(userInput);
             }
+
         } catch (UnknownHostException e) {
             System.err.println("Don't know about host " + ip);
             System.exit(1);
--- src/main/java/codes/dreaming/Server.java
+++ src/main/java/codes/dreaming/Server.java
@@ -1,21 +1,24 @@
 package codes.dreaming;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.io.*;
+import java.net.*;
 import java.util.Random;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class Server {
+    // List to keep all active clients
+    private CopyOnWriteArrayList<ClientHandler> clients;
+
     public void start(int port) {
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             System.out.println("Server has started!");
 
+            clients = new CopyOnWriteArrayList<>();
+
             while (true) {
                 Socket clientSocket = serverSocket.accept();
                 ClientHandler clientHandler = new ClientHandler(clientSocket);
+                clients.add(clientHandler);
                 new Thread(clientHandler).start();
             }
         } catch (IOException e) {
@@ -24,7 +27,27 @@ public class Server {
         }
     }
 
-    private record ClientHandler(Socket socket) implements Runnable {
+    // Function to broadcast a message to all clients
+    private void broadcast(Integer message, ClientHandler notToSend) {
+        for (ClientHandler client : clients) {
+            if (client != notToSend) {
+                client.sendMessage(message);
+            }
+        }
+    }
+
+    private class ClientHandler implements Runnable {
+        private Socket socket;
+        private PrintWriter out;
+
+        public ClientHandler(Socket socket) {
+            this.socket = socket;
+        }
+
+        // Function to send a message to the client
+        public void sendMessage(Integer message) {
+            this.out.println(message);
+        }
 
         @Override
         public void run() {
@@ -32,7 +55,7 @@ public class Server {
                 Random rand = new Random();
                 int number = rand.nextInt(100) + 1;
 
-                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                out = new PrintWriter(socket.getOutputStream(), true);
                 BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
                 String inputLine;
@@ -44,10 +67,14 @@ public class Server {
                         out.println(2);
                     } else {
                         out.println(3);
+                        // Broadcast to all clients that the current client has won
+                        broadcast(4, this);
                         break;
                     }
                 }
 
+                // Remove the client from the active clients list
+                clients.remove(this);
                 socket.close();
             } catch (IOException e) {
                 System.out.println("Exception caught when trying to listen");
