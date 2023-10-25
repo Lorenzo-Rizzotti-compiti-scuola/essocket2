d6c19716a9ee5da5828a8f9a004028013bf5e022
DreamingCodes <me@dreaming.codes>
2023-10-25 11:35:49 +0200

    Implemented multi-thread to handle multiple clients

--- src/main/java/codes/dreaming/Server.java
+++ src/main/java/codes/dreaming/Server.java
@@ -1,39 +1,58 @@
 package codes.dreaming;
 
-import java.io.*;
-import java.net.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.Random;
 
 public class Server {
     public void start(int port) {
-        // Try with resources construct to ensure everything is properly closed
         try (ServerSocket serverSocket = new ServerSocket(port)) {
-
             System.out.println("Server has started!");
 
-            Socket clientSocket = serverSocket.accept();
-
-            Random rand = new Random();
-            int number = rand.nextInt(100) + 1;
-
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-
-            String inputLine;
-            while ((inputLine = in.readLine()) != null) {
-                int guess = Integer.parseInt(inputLine);
-                if (guess < number) {
-                    out.println(1);
-                } else if (guess > number) {
-                    out.println(2);
-                } else {
-                    out.println(3);
-                    break;
-                }
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                ClientHandler clientHandler = new ClientHandler(clientSocket);
+                new Thread(clientHandler).start();
             }
         } catch (IOException e) {
             System.out.println("Exception caught when trying to listen on port " + port);
             System.out.println(e.getMessage());
+        }
+    }
+
+    private record ClientHandler(Socket socket) implements Runnable {
+
+        @Override
+        public void run() {
+            try {
+                Random rand = new Random();
+                int number = rand.nextInt(100) + 1;
+
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+                String inputLine;
+                while ((inputLine = in.readLine()) != null) {
+                    int guess = Integer.parseInt(inputLine);
+                    if (guess < number) {
+                        out.println(1);
+                    } else if (guess > number) {
+                        out.println(2);
+                    } else {
+                        out.println(3);
+                        break;
+                    }
+                }
+
+                socket.close();
+            } catch (IOException e) {
+                System.out.println("Exception caught when trying to listen");
+                System.out.println(e.getMessage());
+            }
         }
     }
 }
